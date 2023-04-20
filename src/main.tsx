5435da95f24de9327d7fb9130d97c1c7a1cc1df8
Pablo Serrano <superpato1234@gmail.com>
2023-04-19 22:41:07 -0600

    fix: add example of counter

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Provider, useDispatch, useSelector } from 'react-redux'
+import {configureStore} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+
+const counterSlice = createSlice(
+  {
+    name: 'counter',
+    initialState: {
+      value: 0,
+    },
+    reducers: {
+      increment: (state)=>{state.value+=1}
+      ,
+      decrement: (state)=>{state.value-=1}
+      ,
+      incrementByAmount: (state,action)=>{state.value+=action.payload}
+    }
+  },
+)
+
+const store = configureStore({reducer:{
+  counter:counterSlice.reducer
+},})
+
+const Prueba = () => {
+  const counter = useSelector((state:any) => state.counter.value)
+  const dispatch = useDispatch();
+
+  return (
+    <>
+    <div>
+        <p>{counter}</p>
+        <button onClick={()=>dispatch(counterSlice.actions.increment())}>+</button>
+        <button  onClick={()=>dispatch(counterSlice.actions.decrement())}>-</button>
+        <button onClick={()=>dispatch(counterSlice.actions.incrementByAmount(3))}>+3</button>
+      </div>
+    </>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <Prueba />
+    </Provider>
   </React.StrictMode>,
 )
